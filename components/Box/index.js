373268a9fec28d339eb78361ce96f2490bcd74b0
Hung Vu <vunamhung@gmail.com>
2020-07-19 22:26:43 +0700

    add container component

--- components/Box/index.js
+++
@@ -1,7 +0,0 @@
-export default function Box({ className, fluid, children, ...props }) {
-  return (
-    <section className={className} {...props}>
-      {!fluid ? <div className="container mx-auto">{children}</div> : children}
-    </section>
-  );
-}
---
+++ components/Container/index.js
@@ -0,0 +1,18 @@
+import { classNames } from "@/lib/helper";
+
+export default function Container({ children, fluid, noGutter, className, ...props }) {
+  return (
+    <div
+      className={classNames(
+        className,
+        { "px-4 ": fluid && !noGutter },
+        { "w-full max-w-none": fluid },
+        { "px-0": noGutter && !fluid },
+        { container: !fluid },
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
