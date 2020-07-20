bda35d33592ab0d18565b302edba9bfd4babd901
Hung Vu <vunamhung@gmail.com>
2020-07-20 23:06:11 +0700

    update :package:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-  theme: {},
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "var(--spacing-4)",
+      },
+    },
+  },
   variants: {},
   plugins: [require("@vunamhung/tailwind-config"), require("@tailwindcss/custom-forms")],
 };
