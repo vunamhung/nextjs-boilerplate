d0e82ea389d10ad16e6f4bf109b2efa796247fe2
Hung Vu <vunamhung@gmail.com>
2020-07-27 12:28:57 +0700

    refine next config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,16 @@
 const withPlugins = require("next-compose-plugins");
-const withOptimizedImages = require("next-optimized-images");
 const withFonts = require("next-fonts");
+const withOptimizedImages = require("next-optimized-images")({
+  mozjpeg: {
+    quality: 90,
+  },
+  webp: {
+    preset: "default",
+    quality: 90,
+  },
+});
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withPlugins([
-  [
-    withOptimizedImages,
-    {
-      mozjpeg: {
-        quality: 90,
-      },
-      webp: {
-        preset: "default",
-        quality: 90,
-      },
-    },
-  ],
-  withFonts,
-  withBundleAnalyzer,
-]);
+module.exports = withPlugins([withOptimizedImages, withFonts, withBundleAnalyzer]);
