79befb0f0291493b7c1ff5f50abe1dc0b70c978b
Hung Vu <vunamhung@gmail.com>
2020-08-07 15:20:18 +0700

    add key

--- modules/Wrapper/index.js
+++ modules/Wrapper/index.js
@@ -16,7 +16,11 @@ export default function Wrapper({ title, hideHeader, hideFooter, children }) {
           as="style"
         />
         <link rel="preload" href="/tailwind.output.css" as="style" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i"
+          key="google-font"
+        />
         <link rel="stylesheet" href="/tailwind.output.css" key="tailwind" />
       </Head>
       {!hideHeader && <HeaderSection sticky />}
