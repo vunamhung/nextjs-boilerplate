3b3345a7a992854090ad6767486ec4a935d194d2
Hung Vu <vunamhung@gmail.com>
2020-08-08 12:43:33 +0700

    fix sep

--- components/Sep/index.js
+++ components/Sep/index.js
@@ -1,3 +1,3 @@
-export default function Sep({ level = 4, ...props }) {
-  return <div className={`h-${level} bg-transparent border-none`} {...props} />;
+export default function Sep({ className = "h-4", ...props }) {
+  return <div className={`${className} bg-transparent border-none`} {...props} />;
 }
