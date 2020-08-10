68535059eab6fca5292055b860ed616bec69c3e8
Hung Vu <vunamhung@gmail.com>
2020-08-10 10:04:05 +0700

    Using Google Analytics with Next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
+import Router from "next/router";
+import * as gtag from "@/lib/gtag";
+
 import "@/styles/index.css";
 import "rc-drawer/assets/index.css";
 import "@glidejs/glide/dist/css/glide.core.min.css";
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    Router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "@/lib/gtag";
 
 export default class CustomDocument extends Document {
   render() {
@@ -14,6 +15,19 @@ export default class CustomDocument extends Document {
           <link rel="preload" href="/tailwind.output.css" as="style" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" />
           <link rel="stylesheet" href="/tailwind.output.css" />
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
