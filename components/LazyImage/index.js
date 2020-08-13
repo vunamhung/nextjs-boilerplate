a0c3c66b053e08e6104d241f8bc9c32c2ab19e3d
Hung Vu <vunamhung@gmail.com>
2020-08-13 12:00:53 +0700

    add lazy image component

---
+++ components/LazyImage/index.js
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import LazyLoad from "vanilla-lazyload";
+import useOnMount from "@/lib/hooks/useOnMount";
+
+export default function LazyImage({ className, placeholder, src, srcset, sizes, width, height, alt }) {
+  useOnMount(() => {
+    if (!document.lazyLoadInstance) {
+      document.lazyLoadInstance = new LazyLoad({
+        elements_selector: ".lazy",
+      });
+    }
+  });
+
+  useEffect(() => {
+    document.lazyLoadInstance.update();
+  });
+
+  // Just render the image with data-src
+  return (
+    <img
+      className={`lazy ${className}`}
+      src={placeholder}
+      data-src={src}
+      data-srcset={srcset}
+      data-sizes={sizes}
+      width={width}
+      height={height}
+      alt={alt}
+    />
+  );
+}
