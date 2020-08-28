7f4654bb8b66d35203461d8a24c2c4e9325f351b
Hung Vu <vunamhung@gmail.com>
2020-08-28 11:31:16 +0700

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,6 @@ module.exports = {
         center: true,
         padding: 'var(--spacing-4)',
       },
-      maxHeight: {
-        '1/4': '25%',
-        '1/2': '50%',
-        '3/4': '75%',
-        '4/5': '80%',
-      },
     },
   },
   variants: {},
