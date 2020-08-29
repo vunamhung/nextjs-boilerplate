d942fd4349aeb34f5f80e952567d00a64498b26e
Hung Vu <vunamhung@gmail.com>
2020-08-29 16:07:49 +0700

    tailwindcss-aspect-ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   purge: ['src/modules/**', 'src/components/**', 'src/pages/**'],
   theme: {
+    aspectRatio: {
+      none: 0,
+      square: [1, 1],
+      '16/9': [16, 9],
+      '4/3': [4, 3],
+      '21/9': [21, 9],
+    },
     extend: {
       container: {
         center: true,
@@ -8,8 +15,10 @@ module.exports = {
       },
     },
   },
-  variants: {},
-  plugins: [require('@vunamhung/tailwind-config'), require('@tailwindcss/custom-forms'), require('@tailwindcss/typography')],
+  variants: {
+    aspectRatio: ['responsive', 'before'],
+  },
+  plugins: [require('@vunamhung/tailwind-config'), require('@tailwindcss/custom-forms'), require('tailwindcss-aspect-ratio')],
   future: {
     removeDeprecatedGapUtilities: true,
   },
