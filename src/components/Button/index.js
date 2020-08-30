2c5ebe18c3cc8d85a02059626dab3c50dae6ffae
Hung Vu <vunamhung@gmail.com>
2020-08-30 09:41:30 +0700

    update css

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,8 +2,15 @@ import Link from 'next/link';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-export default function Button({ children, className, url, tone = 'primary', priority, size, onClick, ...props }) {
-  const classes = classNames('button', className, { [`~${tone}`]: tone, [`!${priority}`]: priority, [`!${size}`]: size });
+export default function Button({ children, className, url, tone = 'primary', priority = 'high', block, outline, size, onClick, ...props }) {
+  const classes = classNames('button', className, {
+    'block w-full': block,
+    'inline-flex w-auto': !block,
+    field: outline,
+    [`~${tone}`]: tone,
+    [`!${priority}`]: priority && !outline,
+    [`!${size}`]: size,
+  });
 
   if (url) {
     return (
@@ -25,6 +32,8 @@ export default function Button({ children, className, url, tone = 'primary', pri
 Button.propTypes = {
   className: PropTypes.string,
   url: PropTypes.string,
+  block: PropTypes.bool,
+  outline: PropTypes.bool,
   tone: PropTypes.oneOf(['gray', 'primary', 'success', 'warning', 'danger', 'info', 'urge']),
   priority: PropTypes.oneOf(['high', 'low']),
   size: PropTypes.oneOf(['small', 'large']),
