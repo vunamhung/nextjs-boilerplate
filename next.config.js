a23e074be1b2207eaf6ded43ab9e960fe80e5ee4
Hung Vu <vunamhung@gmail.com>
2021-04-04 20:15:38 +0700

    refactor

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 const withPlugins = require('next-compose-plugins');
-const withFonts = require('next-fonts');
+const withPWA = require('next-pwa')({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV !== 'production',
+    buildExcludes: [/static\/images\/.*$/],
+  },
+});
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = withPlugins([withFonts], {
+module.exports = withPlugins([withBundleAnalyzer, withPWA], {
   future: {
     webpack5: true,
   },
