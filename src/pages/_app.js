1f250fd404ed7aaac58d57cf0943b33c3d7befd0
Hung Vu <vunamhung@gmail.com>
2021-07-23 10:23:33 +0700

    refactor

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,17 +6,8 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <DefaultSeo
-        openGraph={{
-          type: 'website',
-          locale: 'en_IE',
-          url: 'https://www.url.ie/',
-          site_name: 'SiteName',
-        }}
-        twitter={{
-          handle: '@handle',
-          site: '@site',
-          cardType: 'summary_large_image',
-        }}
+        openGraph={{ type: 'website', locale: 'en_IE', url: 'https://www.url.ie/', site_name: 'SiteName' }}
+        twitter={{ handle: '@handle', site: '@site', cardType: 'summary_large_image' }}
       />
       <Component {...pageProps} />
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import { NextSeo } from 'next-seo';
 
 export default function Home() {
   return (
-    <div>
-      <NextSeo title='Create Next Blog' description='A short description goes here.' />
+    <>
+      <NextSeo title='Home' description='A short description goes here.' />
       <div className='container'>next</div>
-    </div>
+    </>
   );
 }
