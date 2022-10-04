3c047be80c016348a6d30f0d1cf768c2c0c8c425
Hung Vu <vunamhung@gmail.com>
2022-10-04 11:45:01 +0700

    update packages

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
-const withPlugins = require('next-compose-plugins');
-const withPWA = require('next-pwa')({
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV !== 'production',
-    buildExcludes: [/static\/images\/.*$/]
-  }
-});
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
+  enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withPlugins([withBundleAnalyzer, withPWA], {
+module.exports = withBundleAnalyzer({
   webpack(config, { dev, isServer }) {
     // Replace React with Preact only in client production build
     if (!dev && !isServer) {
