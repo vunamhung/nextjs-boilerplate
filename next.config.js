19017a8da25cdda06b53715627e1a58f5c11bf88
Hung Vu <vunamhung@gmail.com>
2022-10-26 10:58:23 +0700

    next 13

--- next.config.js
+++ next.config.js
@@ -2,17 +2,4 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-  webpack(config, { dev, isServer }) {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      });
-    }
-
-    return config;
-  }
-});
+module.exports = withBundleAnalyzer({ experimental: { appDir: true } });
